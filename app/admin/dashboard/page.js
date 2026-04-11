82f4bcf618856538deddf7a4829d5d0d61e21b5c
Hanni <hannihalabi@icloud.com>
2026-04-11 13:28:53 +0200

    Fix build errors: proxy export and data import path

--- app/admin/dashboard/page.js
+++ app/admin/dashboard/page.js
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
-import { groupedProducts, singleProducts } from "@/data/products";
+import { groupedProducts, singleProducts } from "../../../data/products";
 
 const allProducts = [
   ...groupedProducts.flatMap((group) =>
